e73b73b3f4f1549b302e9dec1a540b1f278da6e9
wholemilk <wholemilk25@gmail.com>
2022-04-01 02:41:32 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let captureGraphic;
+
+function setup() {
+  createCanvas(640, 480);
+  captureGraphic = createGraphics(640, 480);
+
+  capture = createCapture(VIDEO);
+  capture.hide();
+}
+
+function draw() {
+  image(capture, 0, 0);
+
+  const code = getCodeFromCapture(capture, captureGraphic);
+  if (code) {
+    print(code.data);
+  }
+}
+
+function getCodeFromCapture(cap, g) {
+  g.image(cap, 0, 0, cap.width, cap.height);
+  const imgData = g.elt
+    .getContext("2d")
+    .getImageData(0, 0, cap.width, cap.height).data;
+
+  return jsQR(imgData, cap.width, cap.height);
+}
